b43108e01fcf37cfeb23a376329dc88d71a26518
Muhammad Angga Adytya <anggaanggaksp@gmail.com>
2025-02-08 23:28:39 +0700

    feat : hotjar setup

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import { Poppins } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 import { Toaster } from "@/components/ui/sonner";
@@ -66,6 +67,24 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning={true}>
+      <head>
+        <Script
+          id="hotjar"
+          // strategy="lazyOnload"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:5297434,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+            `,
+          }}
+        />
+      </head>
       <body className={`${poppins.className} antialiased relative`}>
         <Header />
         <AOSProvider>{children}</AOSProvider>
