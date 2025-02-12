eeba4a598237c318b2659338dae3fa1ba239b31d
Muhammad Angga Adytya <anggaanggaksp@gmail.com>
2025-02-12 10:44:34 +0700

    fix : border avatar

--- common/components/Avatar.jsx
+++ common/components/Avatar.jsx
@@ -2,7 +2,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 export function AvatarSection({ src, alt }) {
   return (
-    <Avatar className="h-24 w-24 md:h-24 md:w-24 lg:h-32 lg:w-32">
+    <Avatar className="h-24 w-24 md:h-24 md:w-24 lg:h-32 lg:w-32 border">
       <AvatarImage src={src} alt={alt} />
       <AvatarFallback>CN</AvatarFallback>
     </Avatar>
